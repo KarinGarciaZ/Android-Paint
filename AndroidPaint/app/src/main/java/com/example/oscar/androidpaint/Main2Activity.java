cedbd2254c8b955d8c0ad4a5815a8cc203d417ef
KarinGarciaZ <oscarin962011@hotmail.com>
2018-05-09 19:44:18 -0500

    Added the icons to the main menu on the second activity.

--- AndroidPaint/app/src/main/java/com/example/oscar/androidpaint/Main2Activity.java
+++ AndroidPaint/app/src/main/java/com/example/oscar/androidpaint/Main2Activity.java
@@ -2,6 +2,8 @@ package com.example.oscar.androidpaint;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_icons, menu);
+        return true;
     }
 }
